4ec9205778f48ee1817301dd5b4f502a0f943467
Shraddha Verma <75242263+ishraddhaverma@users.noreply.github.com>
2023-06-01 17:52:45 +0530

    Add files via upload

---
+++ ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
+
+//viewmodel contains all the data that a view might need as in some cases model obj may not contain all the data
